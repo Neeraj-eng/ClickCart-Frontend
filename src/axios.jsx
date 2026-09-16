dacd286a41cd31858f1b4e3f76559cdf15508752
Neeraj-eng <nirajnagar8085@gmail.com>
2026-09-16 17:16:43 +0530

    Resolve API configuration conflict

--- src/axios.jsx
+++ src/axios.jsx
@@ -1,19 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-<<<<<<< HEAD
-  baseURL:import.meta.env.VITE_API_URL,
-
-  withCredentials : true
-  // baseURL : "http://skillful-dream-production.up.railway.app/api"
-=======
-  baseURL: import.meta.env.VITE_URL,
-
-  withCredentials : true
->>>>>>> 90cbd852b20b11f298963ea7101ca2e0feb966e7
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
 });
 
 delete API.defaults.headers.common["Authorization"];
 
 export default API;
-   
